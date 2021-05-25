37e78c4ad88bcdd1bc848e6e9d93b69c8289fc91
Collin Gilmore <collingilmore@gmail.com>
2021-05-24 22:41:45 -0400

    recipe page now has user google image

--- server.js
+++ server.js
@@ -389,6 +389,8 @@ app.get('/recipe/:id',function(req,res){
   .then((result) => {
     User.findById(result.author) 
     .then((user) => {
+      console.log(user)
+      result.userImage = user.image
       result.userLikes = res.locals.userLikes
       result.userId = res.locals.userId
       result.displayName = capitalize.words(user.displayName)
